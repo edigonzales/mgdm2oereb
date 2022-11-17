8b119b251114183e7368fb8d0eb17ec8fcd8ac52
edigonzales <edi.gonzales@gmail.com>
2022-11-17 20:18:22 +0100

    [WIP]

--- lib/src/main/java/ch/so/agi/mgdm2oereb/Mgdm2Oereb.java
+++ lib/src/main/java/ch/so/agi/mgdm2oereb/Mgdm2Oereb.java
@@ -2,7 +2,30 @@ package ch.so.agi.mgdm2oereb;
 
 public class Mgdm2Oereb {
 
-    public void convert() {
+    public void convert(String xtfFilename, Settings settings) {
+        System.out.println("Hallo Welt.");
+        
+        
+        
         
     }
+    
+    
+    
+    public static final String MODEL = "MODEL"; 
+    
+    public static final String THEME_CODE = "THEME_CODE";
+    
+    public static final String OEREBLEX_HOST = "OEREBLEX_HOST";
+    
+    //public static final String XTF_FILE = "XTF_FILE";
+     
+    public static final String CATALOG = "CATALOG";
+   
+    public static final String OEREBLEX_CANTON = "OEREBLEX_CANTON";
+    
+    public static final String DUMMY_OFFICE_NAME = "DUMMY_OFFICE_NAME";
+    
+    public static final String DUMMY_OFFICE_URL = "DUMMY_OFFICE_URL";
+
 }
--- lib/src/test/java/ch/so/agi/mgdm2oereb/Mgdm2OerebTest.java
+++ lib/src/test/java/ch/so/agi/mgdm2oereb/Mgdm2OerebTest.java
@@ -4,9 +4,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Mgdm2OerebTest {
-    //@Test 
-    void appHasAGreeting() {
-        //App classUnderTest = new App();
-        //assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    @Test 
+    void convertPlanungszonen() {
+        Settings settings = new Settings();
+        settings.setValue(Mgdm2Oereb.MODEL, "Planungszonen_V1_1");
+        settings.setValue(Mgdm2Oereb.THEME_CODE, "ch.Planungszonen");
+        //settings.setValue(Mgdm2Oereb.XTF_FILE, "ch.Planungszonen.sh.mgdm.v1_1.xtf");
+        settings.setValue(Mgdm2Oereb.CATALOG, "ch.sh.OeREBKRMkvs_supplement.xml");
+        
+        Mgdm2Oereb mgdm2oereb = new Mgdm2Oereb();
+        mgdm2oereb.convert("src/test/data/ch.Planungszonen.sh.mgdm.v1_1.xtf", settings);
     }
 }
